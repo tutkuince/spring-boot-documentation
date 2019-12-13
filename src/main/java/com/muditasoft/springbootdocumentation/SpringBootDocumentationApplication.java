e0902771b627b2e73d54852b9774908e59bcde9e
tutkuince <tutku.ince@outlook.com>
2019-12-13 12:16:57 +0300

    initial commit

---
+++ src/main/java/com/muditasoft/springbootdocumentation/SpringBootDocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.muditasoft.springbootdocumentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDocumentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDocumentationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muditasoft/springbootdocumentation/SpringBootDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muditasoft.springbootdocumentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDocumentationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
