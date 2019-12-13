d7281714d4f7b3666c54182e6b08d37ee3f088f5
tutkuince <tutku.ince@outlook.com>
2019-12-13 12:51:29 +0300

    Own custom response messages

--- src/main/java/com/muditasoft/springbootdocumentation/web/LocationController.java
+++ src/main/java/com/muditasoft/springbootdocumentation/web/LocationController.java
@@ -2,6 +2,8 @@ package com.muditasoft.springbootdocumentation.web;
 
 import com.muditasoft.springbootdocumentation.entity.Location;
 import com.muditasoft.springbootdocumentation.service.LocationService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(code=400, message = "This is a bad request, please follow the API documentation for the proper request format."),
+        @ApiResponse(code=401, message = "Due to security constraints, your access request cannot be authorized. "),
+        @ApiResponse(code=500, message = "The server is down. Please make sure that the Location microservice is running.")
+})
+
 public class LocationController {
 
     private final LocationService locationService;
